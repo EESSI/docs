58f38d927287c86e972fa0b9a882b940da402ab3
Caspar van Leeuwen <33718780+casparvl@users.noreply.github.com>
2025-04-17 16:03:50 +0200

    Update scripts/available_software/available_software.py
    Co-authored-by: Bob Dröge <b.e.droge@rug.nl>

--- scripts/available_software/available_software.py
+++ scripts/available_software/available_software.py
@@ -28,7 +28,7 @@ from natsort import natsorted
 EESSI_TOPDIR = "/cvmfs/software.eessi.io/versions/2023.06"
 
 # some CPU targets are excluded for now, because software layer is too incomplete currently
-EXCLUDE_CPU_TARGETS = ['aarch64/a64fx', 'aarch64/nvidia', 'intel/cascadelake', 'intel/icelake']
+EXCLUDE_CPU_TARGETS = ['aarch64/a64fx', 'aarch64/nvidia', 'x86_64/intel/cascadelake', 'x86_64/intel/icelake']
 
 
 # --------------------------------------------------------------------------------------------------------
