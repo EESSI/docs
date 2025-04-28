1e31fd716ff9ca0654423936e9b5e71cde5d9b93
Caspar van Leeuwen <casparl@int5.local.snellius.surf.nl>
2025-04-28 17:04:34 +0200

    Simplify gen_ref_pages by removing commented code and documenting the remaining code

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -7,73 +7,44 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-# need to adjust to the test suite hook
-#root = Path(__file__).parent.parent
-
 TEST_SUITE = "test-suite"
-#src = TEST_SUITE / "eessi"
-MKDOCS_SEARCH_PRIORITY = """---
-search:
-  boost: 0.5
----
-"""
 
 # build a navigation for the menu and a dictionary of navigations for each section
-#menu_nav = mkdocs_gen_files.Nav()
-#navs = {}
 nav = mkdocs_gen_files.Nav()
 
-#for path in sorted(src.rglob("*.py")):
+# Loop through all python files in test-suite/eessi
 for path in sorted(Path(f"{TEST_SUITE}/eessi/").rglob("*.py")):
+    # Get the relative filename, without .py suffix
     module_path = path.relative_to(TEST_SUITE).with_suffix("")
+
+    # define the correspnding (relative) markdown filename
     doc_path = path.relative_to(TEST_SUITE).with_suffix(".md")
+
+    # Specify the full corresponding markdown filename, including a testsuite_api subdir
+    # so that we don't have these API docs scattered in the root of the EESSI docs repo
     full_doc_path = Path("testsuite_api/", doc_path)
 
-    #parts = tuple(module_path.parts)
+    # If something is an __init__, use the directory name as the name of the python module instead of the filename
     parts = list(module_path.parts)
-
-    #if len(parts) > 1 and tuple(parts[:-1]) not in navs:
-    #    navs[tuple(parts[:-1])] = mkdocs_gen_files.Nav()
-
     if parts[-1] == "__init__":
         parts = parts[:-1]
-        #doc_path = doc_path.with_name("index.md")
-        #full_doc_path = full_doc_path.with_name("index.md")
+    # Skip the __main__, if there is one. This is not part of the API
     elif parts[-1] == "__main__":
         continue
 
-    # add item to menu navigation
-    #menu_nav[parts] = doc_path.as_posix()
+    # Add an entry for this module to the navigation
     nav[parts] = doc_path.as_posix()
 
-    # walk through the parents to the item and add it to each navigation section
-    #for i in range(1, len(parts)):
-    #    nav_tuple = tuple(parts[:-i])
-    #    doc_path_relative_to_nav = doc_path.relative_to("/".join(parts[:len(parts)-i])).as_posix()
-    #    navs[nav_tuple][parts] = doc_path_relative_to_nav
-
+    # Create the markdown file
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        #fd.write(MKDOCS_SEARCH_PRIORITY)
+        # identifier is something like eessi.foo.bar
         identifier = ".".join(parts)
-        #fd.write(f"::: {identifier}\n")
         fd.write(f"::: {identifier}")
 
+    # This maps the generated .md file to the source .py, so that you can have an "Edit on GitHub" button
+    # that links to the Python code
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-    # Generate the menu navigation file
+    # Create the api/summary.md file and write the full navigation tree into it so it can be used as a site sidebar
 with mkdocs_gen_files.open("api/summary.md", "w") as nav_file:
-        #nav_file.write(MKDOCS_SEARCH_PRIORITY)
-        #nav_file.writelines(menu_nav.build_literate_nav())
     nav_file.writelines(nav.build_literate_nav())
-
- #Generate the section navigation files, appending to any existing API content
-#for key, nav in navs.items():
-#    fp = '/'.join(key)
-#    filename = f"api/{fp}/index.md"
-#
-#    if not Path(filename).is_file():
-#        with mkdocs_gen_files.open(filename, "w") as nav_file:
-#            nav_file.write(MKDOCS_SEARCH_PRIORITY)
-
-#    with mkdocs_gen_files.open(filename, "a") as nav_file:
-#        nav_file.writelines(nav.build_literate_nav())
