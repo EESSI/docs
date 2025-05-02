0f8992853d96622ede2909c25f66559d566c8b92
laraPPr <lara@edba.be>
2025-05-02 18:37:57 +0200

    fix test_overview_available_software.py script

--- .github/workflows/scripts/test_overview_available_software.py
+++ .github/workflows/scripts/test_overview_available_software.py
@@ -39,26 +39,26 @@ table = soup.find("table", {"class": "table"})
 for row in table.find_all("tr"):
     for column in row.find_all('th'):
         if column.text == "x86_64":
-            print(f'the value for x86_64 is {column.get("colspan")} in the overview page but there are {len(x86_targets)} targets in json_data.json.)
+            print(f'the value for x86_64 is {column.get("colspan")} in the overview page and there are {len(x86_targets)} targets in json_data.json.')
             if int(column.get("colspan")) != len(x86_targets):
                 os.write(2, b'Error: Please make sure the values for x86_64 in json_data.json and overview.md are the same.')
         elif column.text == "aarch64":
-            print(f'the value for aarch64 is {column.get("colspan")} in the overview page but there are {len(ARM_targets)} targets in json_data.json.)
+            print(f'the value for aarch64 is {column.get("colspan")} in the overview page and there are {len(ARM_targets)} targets in json_data.json.')
             if int(column.get("colspan")) != len(ARM_targets):
                 os.write(2, b'Error: Please make sure the values for aarch64 in json_data.json and overview.md are the same.')
         elif column.text == "amd":
-            print(f'the value for amd is {column.get("colspan")} in the overview page but there are {len(amd_targets)} targets in json_data.json.)
+            print(f'the value for amd is {column.get("colspan")} in the overview page and there are {len(amd_targets)} targets in json_data.json.')
             if int(column.get("colspan")) != len(amd_targets):
                 os.write(2, b'Error:  Please make sure the values for amd in json_data.json and overview.md are the same.')
         elif column.text == "intel":
-            print(f'the value for intel is {column.get("colspan")} in the overview page but there are {len(intel_targets)} targets in json_data.json.)
+            print(f'the value for intel is {column.get("colspan")} in the overview page and there are {len(intel_targets)} targets in json_data.json.')
             if int(column.get("colspan")) != len(intel_targets):
                 os.write(2, b'Error:  Please make sure the values for intel in json_data.json and overview.md are the same.')
         elif column.text == "nvidia":
-            print(f'the value for nvidia is {column.get("colspan")} in the overview page but there are {len(nvidia_targets)} targets in json_data.json.)
+            print(f'the value for nvidia is {column.get("colspan")} in the overview page and there are {len(nvidia_targets)} targets in json_data.json.')
             if int(column.get("colspan")) != len(nvidia_targets):
                 os.write(2, b'Error: Please make sure the values for nvidia in json_data.json and overview.md are the same.')
 last_row = table.find_all("tr")[-1]
-print(f'there are {len(last_row.find_all("th"))} columns in the overview page but {len(targets)} targets in json_data.json.')
+print(f'there are {len(last_row.find_all("th"))} columns in the overview page and {len(targets)} targets in json_data.json.')
 if len(last_row.find_all("th")) != len(targets):
     os.write(2, b'Error: Please make sure there are correct number of <th> elements in the last <tr> element in overview.md for JavaScript to generate the table.')
